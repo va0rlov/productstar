45b9211d9b2651720cd6d927d6690e10d187e035
Виктор Орлов <vorlov@topsbi.ru>
2024-11-18 10:28:52 +0300

    'Main' code reviewed: classes separated, "if" constructions reformated

--- src/main/java/com/collections/students/Main.java
+++ src/main/java/com/collections/students/Main.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static StudentCommandHandler STUDENT_COMMAND_HANDLER
-            = new StudentCommandHandler();
+    private static final StudentCommandHandler STUDENT_COMMAND_HANDLER = new StudentCommandHandler();
 
     public static void main(String[] args) {
         while (true) {
-            printMessage();
+            MessagePrinter.printMessage();
             Command command = readCommand();
             if (command.getAction() == Action.EXIT) {
                 return;
-            } else if (command.getAction() == Action.ERROR) {
-                continue;
             } else {
-                STUDENT_COMMAND_HANDLER.processCommand(command);
+                if (command.getAction() != Action.ERROR) {
+                    STUDENT_COMMAND_HANDLER.processCommand(command);
+                }
             }
         }
     }
@@ -34,20 +33,8 @@ public class Main {
                 return new Command(action);
             }
         } catch (Exception ex) {
-            System.out.println("Проблема обработки ввода");
+            System.out.println("Ошибка ввода: " + ex.getMessage());
             return new Command(Action.ERROR);
         }
-    }
-
-    private static void printMessage() {
-        System.out.println("-------------------");
-        System.out.println("0. Выход");
-        System.out.println("1. Создание данных");
-        System.out.println("2. Обновление данных");
-        System.out.println("3. Удаление данных");
-        System.out.println("4. Статистика по курсам");
-        System.out.println("5. Статистика по городам");
-        System.out.println("6. Поиск по фамилии");
-        System.out.println("-------------------");
     }
 }
---
+++ src/main/java/com/collections/students/MessagePrinter.java
@@ -0,0 +1,17 @@
+package com.collections.students;
+
+public class MessagePrinter {
+
+
+    public static void printMessage() {
+        System.out.println("-----------------------------------------------");
+        System.out.println("0. Выход");
+        System.out.println("1. Создание данных");
+        System.out.println("2. Обновление данных");
+        System.out.println("3. Удаление данных");
+        System.out.println("4. Статистика по курсам");
+        System.out.println("5. Статистика по городам");
+        System.out.println("6. Поиск по фамилии");
+        System.out.println("-----------------------------------------------");
+    }
+}
