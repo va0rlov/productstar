1f105bc7d00b3f30be695e4f9a063cf586f8fad2
Виктор Орлов <vorlov@topsbi.ru>
2024-11-18 10:39:25 +0300

    'StudentCommandHandler' code reviewed: classes separated, methods reformated

--- src/main/java/com/collections/students/StudentCommandHandler.java
+++ src/main/java/com/collections/students/StudentCommandHandler.java
@@ -4,62 +4,20 @@ import java.util.Map;
 
 public class StudentCommandHandler {
 
-    private StudentStorage studentStorage = new StudentStorage();
+    private final StudentStorage studentStorage = new StudentStorage();
+
     public void processCommand(Command command) {
         Action action = command.getAction();
 
         switch (action) {
-            case CREATE -> {
-                processCreateCommand(command);
-                break;
-            }
-            case UPDATE -> {
-                processUpdateCommand(command);
-                break;
-            }
-            case DELETE -> {
-                processDeleteCommand(command);
-                break;
-            }
-            case STATS_BY_COURSE -> {
-                processStatsByCourseCommand(command);
-                break;
-            }
-            case STATS_BY_CITY -> {
-                processStatsByCityCommand(command);
-                break;
-            }
-            case SEARCH -> {
-                processSearchCommand(command);
-            }
-            default -> {
-                System.out.println("Действие " + action + " не поддерживается");
-            }
+            case CREATE -> processCreateCommand(command);
+            case UPDATE -> processUpdateCommand(command);
+            case DELETE -> processDeleteCommand(command);
+            case STATS_BY_COURSE -> processStatsByCourseCommand(command);
+            case STATS_BY_CITY -> processStatsByCityCommand(command);
+            case SEARCH -> processSearchCommand(command);
+            default -> System.out.println("Действие " + action + " не поддерживается");
         }
-
-        System.out.println("Обработка команды. Действие: "
-                + command.getAction().name()
-                + ", данные: " + command.getData());
-    }
-
-    private void processSearchCommandWorkShop(Command command) {
-        String surname = command.getData();
-        studentStorage.searchWorkShop(surname);
-    }
-
-    private void processSearchCommand(Command command) {
-        String surname = command.getData();
-        studentStorage.search(surname);
-    }
-
-    private void processStatsByCourseCommand(Command command) {
-        Map<String, Long> data = studentStorage.getCountByCourse();
-        studentStorage.printMap(data);
-    }
-
-    private void processStatsByCityCommand(Command command) {
-        Map<String, Long> data = studentStorage.getCountByCity();
-        studentStorage.printMap(data);
     }
 
     private void processCreateCommand(Command command) {
@@ -90,19 +48,21 @@ public class StudentCommandHandler {
             System.out.printf("Недостаточно сведений для обновления данных: " + dataArray.length + " из " + "6 ");
             return;
         }
-        if (!isNumeric(dataArray[0])) {
+        if (Utils.isNumeric(dataArray[0])) {
             System.out.println("Проблемы обработки введенных данных - идентификатор, д.б. целым числом, повторите ввод");
             return;
         }
         Long id = Long.valueOf(dataArray[0]);
-        if (!isContainsKey(id)) {return;}
+        if (isContainsKey(id)) {
+            return;
+        }
 
         Student student = new Student();
         student.setSurname(dataArray[1]);
         student.setName(dataArray[2]);
         student.setCourse(dataArray[3]);
         student.setCity(dataArray[4]);
-        if (!isNumeric(dataArray[5])) {
+        if (Utils.isNumeric(dataArray[5])) {
             System.out.println("Проблемы обработки введенных данных - возраст, д.б. целым числом, повторите ввод");
             return;
         }
@@ -113,29 +73,41 @@ public class StudentCommandHandler {
     }
 
     private void processDeleteCommand(Command command) {
-
         String data = command.getData();
-        if (!isNumeric(data)) {
+        if (Utils.isNumeric(data)) {
             System.out.println("Проблемы обработки введенных данных - идентификатор, д.б. целым числом, повторите ввод");
             return;
         }
         Long id = Long.valueOf(data);
-        if (!isContainsKey(id)) {return;}
+        if (isContainsKey(id)) {
+            return;
+        }
 
         studentStorage.deleteStudent(id);
         studentStorage.printAll();
     }
 
-    private boolean isNumeric(String string) {
-        return string.matches("\\d+");
+    private void processSearchCommand(Command command) {
+        String surname = command.getData();
+        studentStorage.search(surname);
+    }
+
+    private void processStatsByCourseCommand(Command command) {
+        Map<String, Long> data = studentStorage.getCountByCourse();
+        studentStorage.printMap(data);
+    }
+
+    private void processStatsByCityCommand(Command command) {
+        Map<String, Long> data = studentStorage.getCountByCity();
+        studentStorage.printMap(data);
     }
 
     private boolean isContainsKey(Long id) {
         boolean res = studentStorage.getAll().containsKey(id);
         if (!res) {
-            System.out.println("Студент с данным индентификатором отсутствует, повторите ввод");
+            System.out.println("Студент с данным идентификатором отсутствует, повторите ввод");
             studentStorage.printAll();
         }
-        return res;
+        return !res;
     }
 }
---
+++ src/main/java/com/collections/students/Utils.java
@@ -0,0 +1,8 @@
+package com.collections.students;
+
+public class Utils {
+
+    public static boolean isNumeric(String string) {
+        return !string.matches("\\d+");
+    }
+}
