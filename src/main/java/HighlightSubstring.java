3d80edffcff87566e70eac97ba1388e0f191c45d
Виктор Орлов <vorlov@topsbi.ru>
2024-11-25 19:08:07 +0300

    'docstring && comments' added

--- src/main/java/HighlightSubstring.java
+++ src/main/java/HighlightSubstring.java
@@ -5,11 +5,16 @@ import java.util.logging.Logger;
 
 public class HighlightSubstring {
 
-    // ANSI escape codes для цвета текста
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
 
+    /**
+     * Главный метод программы для выделения подстроки красным цветом в тексте, читаемом с ввода.
+     *
+     * @param args Аргументы командной строки, где первый аргумент - это подстрока для выделения
+     */
     public static void main(String[] args) {
+        // Проверяем, передана ли подстрока в аргументах
         if (args.length < 1) {
             System.out.println("Usage: java HighlightSubstring <substring>");
             return;
@@ -17,14 +22,17 @@ public class HighlightSubstring {
 
         String substring = args[0];
 
+        // Используем BufferedReader для чтения строк из стандартного ввода
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             String line;
+            // Читаем строки по одной и выделяем подстроку красным цветом
             while ((line = reader.readLine()) != null) {
-                // Подсвечиваем все вхождения подстроки
+                // Заменяем каждое вхождение подстроки на выделенную красным
                 String highlightedLine = line.replaceAll(substring, ANSI_RED + substring + ANSI_RESET);
                 System.out.println(highlightedLine);
             }
         } catch (IOException e) {
+            // Логируем исключения ввода-вывода
             Logger.getLogger(HighlightSubstring.class.getName()).log(java.util.logging.Level.SEVERE, null, e);
         }
     }
