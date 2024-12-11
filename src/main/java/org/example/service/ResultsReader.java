630075e619b19ee786c1de0397506b3ec4d5b323
Виктор Орлов <vorlov@topsbi.ru>
2024-12-11 17:36:52 +0300

    'tests' added

--- src/main/java/org/example/service/ResultsReader.java
+++ src/main/java/org/example/service/ResultsReader.java
@@ -1,15 +1,16 @@
 package org.example.service;
 
+import org.example.model.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.example.model.Result;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.nio.file.Files.lines;
 
 @Service
 public class ResultsReader {
@@ -27,11 +28,10 @@ public class ResultsReader {
      * Результаты хранятся в формате: Иван Иванов, М, 10 км, 55:20
      */
     public List<Result> readFromFile(Path filePath) {
-        try {
-            final var collect = Files.lines(filePath)
+        try (var lines = lines(filePath)) {
+            return lines
                     .map(resultParser::parseResult)
                     .collect(Collectors.toList());
-            return collect;
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
