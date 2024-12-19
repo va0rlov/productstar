f1f9685ffa373ba876e896b2a4292cb30fa06851
Виктор Орлов <vorlov@topsbi.ru>
2024-12-19 17:05:35 +0300

    'app' initial commit w/ gradle build

---
+++ src/test/java/ru/contacts/db/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.contacts.db.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
