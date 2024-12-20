fdb09c9f9c8cde84eb3f6435857bcfaa165e5db5
Виктор Орлов <vorlov@topsbi.ru>
2024-12-20 11:48:21 +0300

    'import org.springframework.data repository.ListCrudRepository; ' added

--- src/main/java/ru/contacts/db/app/repository/ContactRepository.java
+++ src/main/java/ru/contacts/db/app/repository/ContactRepository.java
@@ -2,6 +2,7 @@ package ru.contacts.db.app.repository;
 
 import ru.contacts.db.app.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 }
